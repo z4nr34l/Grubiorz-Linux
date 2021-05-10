aa9b022f49f53309363cd69c43d728c0d4bb709a
Mateusz Janota <m.janota@lynth.io>
2021-05-10 10:13:59 +0200

    Mount script update

--- scripts/mount_farm.sh
+++ scripts/mount_farm.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+mkdir /mnt/ORICO1/
+chown nobody:nogroup /mnt/ORICO1
 mount -t cifs -o username=gornik,password=123,domain=WORKGROUP //10.0.200.253/F /mnt/ORICO1/
