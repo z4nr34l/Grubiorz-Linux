23387069f541b527a79c94e4a0f924ab74797866
Mateusz Janota <m.janota@lynth.io>
2021-05-20 10:21:40 +0200

    Added apt package "python-is-python3"

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install python3 python3-dev python3-distutils python3-venv lsb-release -y && exec ./scripts/install_manager.sh && exec ./chia-blockchain/install.sh
+sudo apt install python3 python3-dev python3-distutils python3-venv python-is-python3 lsb-release -y && exec ./scripts/install_manager.sh && exec ./chia-blockchain/install.sh
